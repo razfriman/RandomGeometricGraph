c3f790bf3ca5f5153e0c6a5ea1a8bf074de874a9
Raz Friman <superraz375@yahoo.com>
2015-09-21 12:27:10 -0500

    updates while hovering

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var nodesCount = 40;
+var nodeCount = 40;
 var radius = 200;
 var boundarySize = 500;
 var nodeSize = 10;
@@ -9,13 +9,13 @@ function updateGraph(graph, renderer) {
     graph.clear();
     renderer.reset();
 
-    nodesCount = $('#nodesInput').val();
+    nodeCount = $('#nodesInput').val();
     radius = $('#radiusInput').val();
     boundarySize = $('#boundaryInput').val();
     nodeSize = $('#nodeSizeInput').val();
     nodes = [];
 
-    for(var i = 0; i < nodesCount; ++i) {
+    for(var i = 0; i < nodeCount; ++i) {
         nodes.push({
             id: i,
             x: _.random(0,boundarySize),
@@ -27,9 +27,9 @@ function updateGraph(graph, renderer) {
         graph.addNode(node.id, node);
     });
 
-    for(var i = 0; i < nodesCount; ++i) {
+    for(var i = 0; i < nodeCount; ++i) {
         var point1 = nodes[i];
-        for(var j = 0; j < nodesCount; ++j) {
+        for(var j = 0; j < nodeCount; ++j) {
             if(i == j) continue;
 
             var point2 = nodes[j];
@@ -52,22 +52,80 @@ function onLoad() {
     // highlight all related links
     // when user hovers mouse over a node:
     var highlightRelatedNodes = function(nodeId, isOn) {
-        // just enumerate all related nodes and update link color:
+
+        var baseNodeUI = graphics.getNodeUI(nodeId);
+        if(baseNodeUI) {
+
+            var mainNode = baseNodeUI.childNodes[0];
+            var circleOverlay = baseNodeUI.childNodes[1];
+
+            mainNode.attr('stroke', isOn ? 'green' : 'black')
+
+            if (isOn && !circleOverlay) {
+                baseNodeUI.append(Viva.Graph.svg('circle')
+                    .attr('r', radius)
+                    .attr('pointer-events', 'none')
+                    .attr('fill', 'red')
+                    .attr('opacity', 0.01));
+            } else if (!isOn && circleOverlay) {
+                circleOverlay.remove();
+            }
+        }
+
+            // just enumerate all related nodes and update link color:
         graph.forEachLinkedNode(nodeId, function(node, link){
             var linkUI = graphics.getLinkUI(link.id);
             if (linkUI) {
                 // linkUI is a UI object created by graphics below
                 linkUI.attr('stroke', isOn ? 'red' : 'gray');
             }
+
+            var nodeUI = graphics.getNodeUI(node.id);
+            if(nodeUI) {
+                var nodeRect = nodeUI.childNodes[0];
+                nodeRect.attr('stroke', isOn? 'red' : 'black');
+            }
+
+
+
         });
     };
 
-    $('#nodesInput').val(nodesCount);
+    var params = getQueryParams();
+
+    if(params) {
+        if(params.nodeCount) {
+            nodeCount = params.nodeCount;
+        }
+        if(params.radius) {
+            radius = params.radius;
+        }
+        if(params.boundarySize) {
+            boundarySize = params.boundarySize;
+        }
+        if(params.nodeSize) {
+            nodeSize = params.nodeSize;
+        }
+
+    }
+
+    $('#nodesInput').val(nodeCount);
     $('#radiusInput').val(radius);
     $('#boundaryInput').val(boundarySize);
     $('#nodeSizeInput').val(nodeSize);
 
 
+    var selectedNode = null;
+
+    /*
+    Mobile doesn't accept the click event
+    $('#graphDiv').on('click',function() {
+        console.log('tap');
+        if(selectedNode) {
+            highlightRelatedNodes(selectedNode.id, false);
+        }
+    });
+     */
 
     graphics.node(function(node) {
 
@@ -75,25 +133,33 @@ function onLoad() {
 
         var rect = Viva.Graph.svg('rect')
             .attr('width', nodeSize)
-            .attr('height', nodeSize);
+            .attr('height', nodeSize)
+            .attr('stroke-width', 3);
         ui.append(rect);
 
+        /*
+         Mobile doesn't accept the click event
+        $(rect).click(function(event) {
+            highlightRelatedNodes(node.id, true);
+            selectedNode = node;
+            event.stopPropagation();
+            return true;
+        });
+        */
+        
         $(rect).hover(function() { // mouse over
             highlightRelatedNodes(node.id, true);
 
             ui.append(Viva.Graph.svg('circle')
                 .attr('r', radius)
                 .attr('pointer-events', 'none')
-                .attr('fill', 'green')
-                .attr('opacity', 0.5));
+                .attr('fill', 'red')
+                .attr('opacity', 0.1));
 
 
 
         }, function() { // mouse out
             highlightRelatedNodes(node.id, false);
-
-
-            console.log(ui);
             ui.childNodes[1].remove();
 
         });
@@ -146,4 +212,25 @@ function getDistance(x1,y1,x2,y2) {
     var x = x2-x1;
     var y = y2-y1;
     return Math.sqrt(x*x + y*y);
+}
+
+/**
+ * Parse query string.
+ * ?a=b&c=d to {a: b, c: d}
+ * @param {String} (option) queryString
+ * @return {Object} query params
+ */
+function getQueryParams(queryString) {
+    var query = (queryString || window.location.search).substring(1); // delete ?
+    if (!query) {
+        return false;
+    }
+    return _
+        .chain(query.split('&'))
+        .map(function(params) {
+            var p = params.split('=');
+            return [p[0], decodeURIComponent(p[1])];
+        })
+        .object()
+        .value();
 }
